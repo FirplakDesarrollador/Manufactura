7d7766f28bcb0554ac5c09793afb195ff99a11e5
FirplakDesarrollador <analista2.desarrollo@firplak.com>
2026-08-24 12:54:54 -0500

    fix: remover alerta de guardado exitoso en calidad ms

--- app/calidad/ms/page.tsx
+++ app/calidad/ms/page.tsx
@@ -201,7 +201,6 @@ export default function CalidadMsReportPage() {
             setSelectedDefects({})
             setSelectedProduct('')
             setPhotoFile(null)
-            alert('Reporte guardado exitosamente')
         }
     }
 
